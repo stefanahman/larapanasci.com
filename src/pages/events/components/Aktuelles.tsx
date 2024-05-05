71161ea92ae86ede021e7c82200ab438f07a9519
Stefan Åhman <stefan.ahman@gmail.com>
2024-05-05 21:02:24 +0300

    Add new event

--- src/pages/events/components/Aktuelles.tsx
+++ src/pages/events/components/Aktuelles.tsx
@@ -4,6 +4,7 @@ import floral1 from '@/assets/floral-design1.svg'
 import round from '@/assets/round-2.png'
 
 import event3 from '@/assets/event-3.jpg'
+import event4 from '@/assets/event-4.jpg'
 
 export const Aktuelles = () => {
   return (
@@ -11,6 +12,7 @@ export const Aktuelles = () => {
       <div className="container lg:flex">
         <div className="text-center lg:w-1 lg:text-left pb-10 lg:pb-0">
           <h2 className="text-center lg:pb-12 pb-12">Aktuelles</h2>
+          <img className="mx-auto md:w-1/2 mb-8" src={event4} alt="event" />
           <img className="mx-auto md:w-1/2" src={event3} alt="event" />
         </div>
       </div>
