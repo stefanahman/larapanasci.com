8773abb94b24ae198b967a2ad1bbd8c52eef8b58
Stefan Åhman <stefan.ahman@gmail.com>
2024-05-30 00:48:07 +0200

    Add events

--- src/pages/events/components/Aktuelles.tsx
+++ src/pages/events/components/Aktuelles.tsx
@@ -5,6 +5,8 @@ import round from '@/assets/round-2.png'
 
 import event3 from '@/assets/event-3.jpg'
 import event4 from '@/assets/event-4.jpg'
+import event6 from '@/assets/event-6.jpg'
+import event7 from '@/assets/event-7.jpg'
 
 export const Aktuelles = () => {
   return (
@@ -12,8 +14,15 @@ export const Aktuelles = () => {
       <div className="container lg:flex">
         <div className="text-center lg:w-1 lg:text-left pb-10 lg:pb-0">
           <h2 className="text-center lg:pb-12 pb-12">Aktuelles</h2>
-          <img className="mx-auto md:w-1/2 mb-8" src={event4} alt="event" />
-          <img className="mx-auto md:w-1/2" src={event3} alt="event" />
+          <img className="mx-auto md:w-1/2 mb-8" src={event6} alt="event" />
+          <img className="mx-auto md:w-1/2 mb-8" src={event7} alt="event" />
+          <h3 className="mx-auto md:w-1/2 lg:pb-4 pb-4 text-p-2 text-center">
+            - Ausverkauft -
+          </h3>
+          <div className="mx-auto md:w-1/2" style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+            <img className="lg:w-1/2" src={event4} alt="event" />
+            <img className="lg:w-1/2" src={event3} alt="event" />
+          </div>
         </div>
       </div>
     </section>
