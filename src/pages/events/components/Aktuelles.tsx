cfeefceec92495bf617c071ccdc141d3c676b683
Stefan Åhman <stefan.ahman@gmail.com>
2024-07-31 18:06:10 +0200

    Update events page

--- src/pages/events/components/Aktuelles.tsx
+++ src/pages/events/components/Aktuelles.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import floral1 from '@/assets/floral-design1.svg'
 import round from '@/assets/round-2.png'
 
 import event3 from '@/assets/event-3.jpg'
@@ -14,17 +13,15 @@ export const Aktuelles = () => {
       <div className="container lg:flex">
         <div className="text-center lg:w-1 lg:text-left pb-10 lg:pb-0">
           <h2 className="text-center lg:pb-12 pb-12">Aktuelles</h2>
-          <p className="text-p-2 text-center pb-10 lg:w-1/2 mx-auto">
-            Halten Sie Ausschau nach neuen bevorstehenden Veranstaltungen.
+          <p className='text-p-2 mb-12 text-center'>
+            Keine aktuellen Events
           </p>
           <h2 className="text-center lg:pb-12 pb-12 text-p-2">Frühere</h2>
-          <div className="mx-auto pb-3 md:w-1/2" style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-            <img className="lg:w-1/2" src={event6} alt="event" />
-            <img className="lg:w-1/2" src={event7} alt="event" />
-          </div>
-          <div className="mx-auto pb-3 md:w-1/2" style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-            <img className="lg:w-1/2" src={event4} alt="event" />
-            <img className="lg:w-1/2" src={event3} alt="event" />
+          <div className='flex flex-row' style={{gap: 8, overflowX: 'auto'}}>
+            <img className="" style={{width: '18rem'}} src={event6} alt="event" />
+            <img className="" style={{width: '18rem'}} src={event7} alt="event" />
+            <img className="" style={{width: '18rem'}} src={event4} alt="event" />
+            <img className="" style={{width: '18rem'}} src={event3} alt="event" />
           </div>
         </div>
       </div>
--- src/pages/events/components/Visionen.tsx
+++ src/pages/events/components/Visionen.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
 
-import floral1 from '@/assets/floral-design1.svg'
 import round from '@/assets/round-2.png'
 
 import event1 from '@/assets/event-1.jpg'
 import event2 from '@/assets/event-2.jpg'
-import event5 from '@/assets/event-5.jpg'
 
 export const Visionen = () => {
   return (
     <section style={{ backgroundImage: `url(${round})` }} className="bg-cream lg:py-20 md:py-16 px-4 py-14 bg-no-repeat bg-[90%_100%] bg-6">
       <div className="container lg:flex">
-        <div className="text-center lg:w-1/2 lg:text-left lg:pr-20 pb-10 lg:pb-0">
-          <h2 className="lg:pb-12 pb-12">Entstehende Visionen</h2>
-          <ul className="space-y-4 lg:pb-0 pb-0">
-            <li className="flex items-start"><img className="pt-1 pr-3" src={floral1} alt="icon-floral" />
-              Outdoor “Workshop-Day” in Tipi-Zelt <br/>
-            </li>
-            <li className="flex items-start">
-              <img className="mx-auto" src={event5} alt="event" />
-            </li>
-            <li className="flex items-start"><img className="pt-1 pr-3" src={floral1} alt="icon-floral" />
-              Outdoor Makramé & Handpan
-            </li>
-            <li className="flex items-start"><img className="pt-1 pr-3" src={floral1} alt="icon-floral" />
-              Wein Degustation, Töpfern & Sound
-            </li>
-            <li className="flex items-start"><img className="pt-1 pr-3" src={floral1} alt="icon-floral" />
-              Creative Retreat - Ticino
-            </li>
-          </ul>
-        </div>
         <div className="lg:w-1/2 lg:pb-0 pb-0 space-y-4">
           <img className="mx-auto" src={event1} alt="event" />
           <img className="mx-auto" src={event2} alt="event" />
